d892385a224fc4bfc6b44498696af01f09f6c871
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-04-15 17:42:31 -0500

    referse single word Jest TDD

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+// // Strings in JavaScript cannot be reversed directly 
+// so you're going to have to split it into something else first.. 
+// do the reversal and then join it back together into a string.
+const reverseString = function (str) {
+  const inputString = str.split('');
+  let i = inputString.length - 1;
+  let outputString = '';
+  for (i; i >= 0; i--) {
+    outputString += inputString.[i];
+  }
+  return outputString;
 };
 
 // Do not edit below this line
