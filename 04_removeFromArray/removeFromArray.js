9803f6e5f48d3de36b20b22d7ed3e36312d167da
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-04-15 23:15:30 -0500

    remove single argument value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, remove) {
+  const newArray = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === remove) {
+      continue;
+    }
+    else if (array[i] !== remove) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
