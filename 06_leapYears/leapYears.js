c076717d4bfcb10f18d5bf0f1882790b369ffc3f
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-04-17 00:40:04 -0500

    pass all leapYears Jest TDD tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if ((year % 400 === 0) && (year % 100 === 0)) {
+    //leap year
+    return true;
+  }
+  else if ((year % 4 === 0) && (year % 100 === 0)) {
+    //not leap year
+    return false;
+  }
+  //leap year
+  else if (year % 4 === 0) return true;
+  // not leap year
+  else return false;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
