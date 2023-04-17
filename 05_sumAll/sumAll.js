ee343e88ac9e7b3b28b7e8ab70efd42373b4d526
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-04-16 22:08:02 -0500

    sum numbers within the range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (intOne, intTwo) {
+  let sum = 0;
+  for (intOne; intOne <= intTwo; intOne++) {
+    sum += intOne;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
