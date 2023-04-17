17226f3244e260405249894af84860d2cd31239c
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-04-16 22:30:33 -0500

    works with larger number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,18 @@
 const sumAll = function (intOne, intTwo) {
   let sum = 0;
-  for (intOne; intOne <= intTwo; intOne++) {
-    sum += intOne;
+  if ((intOne < 0 || intTwo < 0)) {
+    return 'ERROR';
   }
+  else if (intOne > intTwo) {
+    for (intOne; intOne >= intTwo; intOne--) {
+      sum += intOne;
+    }
+    return sum;
+  }
+  else
+    for (intOne; intOne <= intTwo; intOne++) {
+      sum += intOne;
+    }
   return sum;
 };
 // Do not edit below this line
