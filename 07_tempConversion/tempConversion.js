c7884a1925289473c885053716479472d1427a28
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-04-17 10:25:18 -0500

    pass all tempConversion Jest TDD tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const converted = (fahrenheit - 32) * 5 / 9;
+  let fixedRound = Math.round(converted * 10) / 10;
+  return fixedRound;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const converted = celsius * 9 / 5 + 32
+  let fixedRound = Math.round(converted * 10) / 10;
+  return fixedRound;
 };
 
 // Do not edit below this line
