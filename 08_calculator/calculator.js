a05c0ecdd2e5edbd4139278052f5f2d3def480e4
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-05-06 15:31:02 -0500

    Pass all array factorial Jest TDD tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,12 @@ const power = function (x, y) {
   return x ** y;
 };
 
-const factorial = function () {
-
+const factorial = function (inputNumber) {
+  let myArray = [];
+  for (let i = 1; i <= inputNumber; i++) {
+    myArray.push(i);
+  }
+  return myArray.reduce((acc, curr) => acc * curr, 1);
 };
 
 // Do not edit below this line
