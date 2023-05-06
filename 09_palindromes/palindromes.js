e6fbec3dc7e502d7b27ecb479c4fcac4bd0ac15a
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-05-06 18:10:14 -0500

    Pass all palindrome Jest TDD tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,11 @@
 const palindromes = function (string) {
   let lowerCaseStr = string.toLowerCase();
-  let myArr = lowerCaseStr.split();
+  let noPuncStr = lowerCaseStr.replace(/[^a-z]/g, "")
+  let myArr = noPuncStr.split('');
   let reversedArr = myArr.slice().reverse();
-  let reversedStr = reversedArr.toString();
-  if (lowerCaseStr == reversedStr) return true;
+  let reversedStr = reversedArr.join('');
+  if (noPuncStr == reversedStr) return true;
+  else return false;
 };
 
 // Do not edit below this line
