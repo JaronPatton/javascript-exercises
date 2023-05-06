0a876f1337122e1edbcc3072bd2584a5131fe490
jaronpatton <34529224+JaronPatton@users.noreply.github.com>
2023-05-06 17:24:21 -0500

    Palindrome function works with single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let lowerCaseStr = string.toLowerCase();
+  let myArr = lowerCaseStr.split();
+  let reversedArr = myArr.slice().reverse();
+  let reversedStr = reversedArr.toString();
+  if (lowerCaseStr == reversedStr) return true;
 };
 
 // Do not edit below this line
